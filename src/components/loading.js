dab90c3bc49aafb2454b92c995a83ec53add906c
lil Wolf <59448133+lilWolf011@users.noreply.github.com>
2024-11-20 18:56:36 +0300

    Update loading.js

--- src/components/loading.js
+++ src/components/loading.js
@@ -5,19 +5,19 @@ import Link from 'next/link';
 
 export default function Loading() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen sm:p-20">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-svh sm:p-20">
       <main className="space-y-4 row-start-2 items-center sm:items-start w-full max-w-[350px]">
         <div className="flex flex-col items-center w-full">
           <Image
             src={instagram}
-            width={100}
-            height={100}
+            width={80}
+            height={80}
             alt="Instagram Icon"
           />
         </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center text-xs text-[#A8A8A8]">
-        <Image src={meta} width={110} height={40} alt="Picture of the author" />
+        <Image src={meta} width={75} height={40} alt="Picture of the author" />
       </footer>
     </div>
   );
